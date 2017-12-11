384ca3f6f7a07cab967f561042a01949bd04fad1
Riccardo Cirimelli <rcirimelli@gmail.com>
2017-12-11 23:02:02 +0100

    day 11 + refactoring

--- src/aoc_11_hex_ed.py
+++ src/aoc_11_hex_ed.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import Dict, List
 
-from src.utils import read_first_line
+from src.utils import read_first_line, compute_coordinates_from_steps, Step, Points, Point
+from functools import reduce
 
 """
 --- Day 11: Hex Ed ---
@@ -35,14 +36,77 @@ For example:
  - ne,ne,sw,sw is 0 steps away (back where you started).
  - ne,ne,s,s is 2 steps away (se,se).
  - se,sw,se,sw,sw is 3 steps away (s,s,sw).
+ 
+--- Part Two ---
+
+How many steps away is the furthest he ever got from his starting 
+position?
 """
+
+direction_to_step: Dict[str, Step] = {
+    'n': (1, 0),
+    'ne': (1, -1),
+    'se': (0, -1),
+    's': (-1, 0),
+    'sw': (-1, 1),
+    'nw': (0, 1)
+}
+
+
+def compute_final_distance(points: Points) -> int:
+    return hex_distance((0, 0), points[-1])
+
+
+def compute_max_distance(point: Points) -> int:
+    return max(hex_distance((0, 0), p) for p in point)
+
+
+def compute_positions(directions: List[str]) -> Points:
+    steps = (direction_to_step[d] for d in directions)
+    return reduce(compute_next_position, steps, [(0, 0)])
+
+
+def compute_next_position(acc: Points, step: Step) -> Points:
+    start = acc[-1]
+    position = compute_coordinates_from_steps(start, step)
+    acc.append(position)
+    return acc
 
 
 def parse_raw_input(raw: str) -> List[str]:
     return raw.split(',')
 
 
+def hex_distance(p1: Point, p2: Point) -> int:
+    x1, y1 = p1
+    x2, y2 = p2
+
+    dx = x2 - x1
+    dy = y2 - y1
+
+    if sign(dx) == sign(dy):
+        d = abs(dx + dy)
+    else:
+        d = max(abs(dx), abs(dy))
+    return d
+
+
+def sign(x: int) -> int:
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
+
+
 if __name__ == '__main__':
     raw_puzzle_input = read_first_line('hex_ed.txt')
     puzzle_input = parse_raw_input(raw_puzzle_input)
-    print(puzzle_input)
+
+    positions = compute_positions(puzzle_input)
+    final_distance = compute_final_distance(positions)
+    max_distance = compute_max_distance(positions)
+
+    print(f'Result for Part 1: {final_distance}')
+    print(f'Result for Part 2: {max_distance}')
--- src/test_11_hex_ed.py
+++ src/test_11_hex_ed.py
@@ -1,6 +1,15 @@
-"""
-ne,ne,ne is 3 steps away.
-ne,ne,sw,sw is 0 steps away (back where you started).
-ne,ne,s,s is 2 steps away (se,se).
-se,sw,se,sw,sw is 3 steps away (s,s,sw).
-"""
+from src.aoc_11_hex_ed import parse_raw_input, compute_final_distance, compute_positions
+
+
+def test_part_1():
+    test_cases = [
+        ('ne,ne,ne', 3),
+        ('ne,ne,sw,sw', 0),
+        ('ne,ne,s,s', 2),
+        ('se,sw,se,sw,sw', 3)
+    ]
+
+    for puzzle_raw_input, expected_output in test_cases:
+        puzzle_input = parse_raw_input(puzzle_raw_input)
+        positions = compute_positions(puzzle_input)
+        assert compute_final_distance(positions) == expected_output
